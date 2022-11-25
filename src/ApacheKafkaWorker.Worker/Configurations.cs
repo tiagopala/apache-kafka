364341d6dece404fbb0182ba57a612466d3eccd9
Tiago Pala <tiago_pala@outlook.com>
2022-11-25 09:06:16 -0300

    Início implementação novo Worker

---
+++ src/ApacheKafkaWorker.Worker/Configurations.cs
@@ -0,0 +1,12 @@
+﻿namespace ApacheKafkaWorker.Worker
+{
+    internal static class Configurations
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddHostedService<KafkaWorker>();
+
+            return services;
+        }
+    }
+}
