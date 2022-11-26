d7ec5177c5380050c1c63148c6ebaa7e22353620
Tiago Pala <tiago_pala@outlook.com>
2022-11-26 11:30:41 -0300

    Refatoração do consumidor utilizado para um worker genérico capaz de processar diferentes eventos

---
+++ src/ApacheKafkaWorker.Infrastructure/Avros/AvroSerializer.cs
@@ -0,0 +1,11 @@
+﻿using Confluent.Kafka;
+using System.Text.Json;
+
+namespace ApacheKafkaWorker.Infrastructure.Avros
+{
+    public class AvroSerializer<T> : ISerializer<T>
+    {
+        public byte[] Serialize(T data, SerializationContext context)
+            => JsonSerializer.SerializeToUtf8Bytes(data);
+    }
+}
---
+++ src/ApacheKafkaWorker.Producer/OnboardingEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace ApacheKafkaWorker.Producer;
+
+internal class OnboardingEvent
+{
+    public string Id { get; set; }
+    public string Description { get; set; }
+}
